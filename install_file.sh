3c4e4daa965a168e4d6cf425dfb8e114e232940b
sheyestherrr <72214959+sheyestherrr@users.noreply.github.com>
2025-08-01 16:13:03 +0000

    install

---
+++ install_file.sh
@@ -0,0 +1,5 @@
+wget https://eac866-708333578.antpcdn.com:19001/b/pkg-ant.baidu.com/issue/netdisk/LinuxGuanjia/4.17.7/baidunetdisk_4.17.7_amd64.deb
+
+sudo dpkg -i baidunetdisk_4.17.7_amd64.deb
+
+sudo apt-get update && sudo apt-get install filezilla -y && sudo apt-get install filezilla-common -y
